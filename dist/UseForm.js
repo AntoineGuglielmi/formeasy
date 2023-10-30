69dcaf4aa08884ad3b70e3002ec742d6770f1f88
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-10-31 00:27:02 +0100

    [master] Forgot to build

---
+++ dist/UseForm.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UseForm = void 0;
+const vue_1 = require("vue");
+const index_1 = require("./index");
+const UseForm = () => {
+    const errors = (0, vue_1.ref)({});
+    // ########################################################################################################################
+    const getErrors = (field) => {
+        if (field) {
+            return errors.value[field] || [];
+        }
+        return errors.value;
+    };
+    const getError = (field) => {
+        const errors = getErrors(field);
+        if (Array.isArray(errors)) {
+            return errors[0] || null;
+        }
+        return null;
+    };
+    const formIsValid = (form) => {
+        errors.value = {};
+        const formValues = (0, index_1.getFormValues)(form);
+        Object.entries(form).map(([name, { value, validationRules = [] }]) => {
+            const validationRulesResults = validationRules.reduce((acc, validationRule) => {
+                const validationRuleResult = validationRule({ value, formValues });
+                acc.push(...(validationRuleResult !== true ? [String(validationRuleResult)] : []));
+                return acc;
+            }, []);
+            if (validationRulesResults.length) {
+                errors.value[name] = validationRulesResults;
+            }
+        });
+        return Object.keys(errors.value).length === 0;
+    };
+    // ########################################################################################################################
+    return {
+        getErrors,
+        getError,
+        formIsValid,
+    };
+};
+exports.UseForm = UseForm;
---
+++ dist/validationRules.js
@@ -0,0 +1,72 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pattern = exports.matches = exports.minLength = exports.onlyLetters = exports.required = void 0;
+const src_1 = require("../src");
+/**
+ * required
+ * @description Required validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value}: {value: any}) => boolean | string}
+ */
+const required = (params = {}) => {
+    const _params = Object.assign({ message: 'This field is required' }, params);
+    const { special = null } = _params;
+    return ({ value }) => {
+        const isRequired = special !== null ? special : value;
+        return !!isRequired || (0, src_1.getMessage)(_params);
+    };
+};
+exports.required = required;
+/**
+ * onlyLetters
+ * @description Only letters validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value}: {value: any}) => boolean | string}
+ */
+const onlyLetters = (params = {}) => {
+    const _params = Object.assign({ message: 'This field must contain only letters', required: true }, params);
+    const { required } = _params;
+    return ({ value }) => {
+        const matches = required ? /^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(value) : /^[A-Za-zÀ-ÖØ-öø-ÿ]*$/.test(value);
+        return matches || (0, src_1.getMessage)(_params);
+    };
+};
+exports.onlyLetters = onlyLetters;
+/**
+ * minLength
+ * @description Min length validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value}: {value: any}) => boolean | string}
+ */
+const minLength = (params = {}) => {
+    const _params = Object.assign({ message: 'This field must contain at least {min} characters', min: 3 }, params);
+    const { min } = _params;
+    return ({ value }) => {
+        return (value.length >= min) || (0, src_1.getMessage)(_params);
+    };
+};
+exports.minLength = minLength;
+/**
+ * maxLength
+ * @description Max length validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value, formValues}: {value: any, formValues?: any}) => boolean | string}
+ */
+const matches = (params = {}) => {
+    const _params = Object.assign({ message: 'This field must match "{field}" field' }, params);
+    const { field } = _params;
+    return ({ value, formValues }) => {
+        const matches = value === formValues[field];
+        return matches || (0, src_1.getMessage)(_params);
+    };
+};
+exports.matches = matches;
+const pattern = (params = {}) => {
+    const _params = Object.assign({ message: 'This field is not valid' }, params);
+    const { pattern } = _params;
+    return ({ value }) => {
+        const matches = pattern.test(value);
+        return matches || (0, src_1.getMessage)(_params);
+    };
+};
+exports.pattern = pattern;
