f95f6cddfd900231385d41ec12230ffc675f48d6
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-11-05 00:06:16 +0100

    [master] Create other validation rules

--- src/validationRules.ts
+++ src/validationRules.ts
@@ -1,5 +1,7 @@
 import {
   getMessage,
+  TValidationRule,
+  TValidationRuleResult,
   TValidationRuleWrapperParams,
 } from './index';
 
@@ -17,10 +19,11 @@ export const required = (params: TValidationRuleWrapperParams = {}) => {
   const {
     special = null
   } = _params
-  return ({ value }: { value: any }) => {
+  const required = ({ value }: { value: any }): TValidationRuleResult => {
     const isRequired = special !== null ? special : value
     return !!isRequired || getMessage(_params)
   }
+  return required
 }
 
 /**
@@ -38,8 +41,29 @@ export const onlyLetters = (params: TValidationRuleWrapperParams = {}) => {
   const {
     required
   } = _params
-  return ({ value }: { value: any }) => {
+  return ({ value }: { value: any }): TValidationRuleResult => {
     const matches = required ? /^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(value) : /^[A-Za-zÀ-ÖØ-öø-ÿ]*$/.test(value)
+    return matches || getMessage(_params)
+  }
+}
+
+/**
+ * onlyNumbers
+ * @description Only numbers validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value}: {value: any}) => any}
+ */
+export const onlyNumbers = (params: TValidationRuleWrapperParams = {}) => {
+  const _params: TValidationRuleWrapperParams = {
+    message: 'This field must contain only numbers',
+    required: true,
+    ...params
+  }
+  const {
+    required
+  } = _params
+  return ({ value }: { value: any }): TValidationRuleResult => {
+    const matches = required ? /^[0-9]+$/.test(value) : /^[0-9]*$/.test(value)
     return matches || getMessage(_params)
   }
 }
@@ -59,8 +83,28 @@ export const minLength = (params: TValidationRuleWrapperParams = {}) => {
   const {
     min
   } = _params
-  return ({ value }: { value: any }) => {
+  return ({ value }: { value: any }): TValidationRuleResult => {
     return (value.length >= min) || getMessage(_params)
+  }
+}
+
+/**
+ * maxLength
+ * @description Max length validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value}: {value: any}) => boolean | string}
+ */
+export const maxLength = (params: TValidationRuleWrapperParams = {}) => {
+  const _params: TValidationRuleWrapperParams = {
+    message: 'This field must contain at most {max} characters',
+    max: 10,
+    ...params
+  }
+  const {
+    max
+  } = _params
+  return ({ value }: { value: any }): TValidationRuleResult => {
+    return (value.length <= max) || getMessage(_params)
   }
 }
 
@@ -78,12 +122,18 @@ export const matches = (params: TValidationRuleWrapperParams = {}) => {
   const {
     field
   } = _params
-  return ({value, formValues}: { value: any, formValues?: any }) => {
+  return ({value, formValues}: { value: any, formValues?: any }): TValidationRuleResult => {
     const matches = value === formValues[field]
     return matches || getMessage(_params)
   }
 }
 
+/**
+ * pattern
+ * @description Pattern validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value}: {value: any}) => any}
+ */
 export const pattern = (params: TValidationRuleWrapperParams = {}) => {
   const _params: TValidationRuleWrapperParams = {
     message: 'This field is not valid',
@@ -92,8 +142,25 @@ export const pattern = (params: TValidationRuleWrapperParams = {}) => {
   const {
     pattern
   } = _params
-  return ({ value }: { value: any }) => {
+  return ({ value }: { value: any }): TValidationRuleResult => {
     const matches = pattern.test(value)
     return matches || getMessage(_params)
   }
+}
+
+/**
+ * isEmail
+ * @description Email validation rule
+ * @param {TValidationRuleWrapperParams} params
+ * @returns {({value}: {value: any}) => any}
+ */
+export const isEmail = (params: TValidationRuleWrapperParams = {}) => {
+  const _params: TValidationRuleWrapperParams = {
+    message: 'This field must be a valid email',
+    ...params
+  }
+  return pattern({
+    pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    ..._params
+  })
 }
